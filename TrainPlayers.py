98fcec4d7daed77574703bffd98d1afc9f3f7d51
Simon Aertssen <Simonaertssen@gmail.com>
2020-09-06 12:17:26 +0200

    Made a readable format, ready to train multiple players

--- TrainPlayers.py
+++ TrainPlayers.py
@@ -2,84 +2,72 @@ import matplotlib.pyplot as plt
 from Players import RandomPlayer, MiniMaxPlayer, AlphaBetaPlayer, QPlayer, NeuralPlayer
 from Game import TicTacToe
 
-from skopt import gp_minimize
-
 import sys
 import numpy as np
 
-def objective(args):
-    lr, decay, exploration = args
-    p1 = QPlayer(lr, decay, exploration)
-    p2 = RandomPlayer()
+def train(player1, player2, rounds=10, battles=10):
+    game = TicTacToe(player1, player2)
+    count, player1wins, player2wins, draws = game.train(rounds, battles)
 
-    game = TicTacToe(p1, p2)
-    count, p1wins, p2wins, draws = game.train(100, 100)
-    score = sum(p1wins) + sum(draws) - sum(p2wins)
-    return -score
+    plt.figure()
+    plt.ylabel('Game outcomes in %')
+    plt.xlabel('Game number')
 
-res = gp_minimize(objective, [(0.0, 1.0), (0.0, 1.0), (0.0, 1.0)], acq_func="LCB", n_calls=15, n_random_starts=5, noise=0.1**2)
-print("lr={}, decay={}, exploration={}, f()={}".format(res.x[0], res.x[1], res.x[2], res.fun))
+    plt.plot(count, player1wins, 'g-', label='{} wins'.format(player1.name))
+    plt.plot(count, player2wins, 'b-', label='{} wins'.format(player2.name))
+    plt.plot(count, draws, 'r-', label='Draw')
 
-p1 = QPlayer(lr=0.4905593711696319, decay=0.39505136184649137, exploration=0.0)
-p2 = RandomPlayer()
+    plt.grid(True)
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5,1.12), shadow=False, fancybox=True, framealpha=0.3, ncol=3)
+    plt.show()
 
-game = TicTacToe(p1, p2)
-count, p1wins, p2wins, draws = game.train(100, 100)
+# Working QPlayer versus RandomPlayer
+p1 = NeuralPlayer(lr=0.1)
+p2 = QPlayer(lr=0.9, lrdecay=0.9, exploration=1.0, explorationdecay=0.9)
 
-plt.figure()
-plt.ylabel('Game outcomes in %')
-plt.xlabel('Game number')
-
-plt.plot(count, p1wins, 'g-', label='{} wins'.format(p1.name))
-plt.plot(count, p2wins, 'b-', label='{} wins'.format(p2.name))
-plt.plot(count, draws, 'r-', label='Draw')
-
-plt.grid(True)
-plt.legend(loc='best', shadow=True, fancybox=True, framealpha=0.7)
-plt.show()
+train(p1, p2, rounds=10, battles=10)
 
 sys.exit()
 
-p1 = QPlayer(lr=0.9, decay=0.95, exploration=0.1)
+# Non-working QPlayer versus QPlayer
+p1 = QPlayer(lr=0.9, lrdecay=0.9, exploration=1.0, explorationdecay=0.9)
+p2 = QPlayer(lr=0.9, lrdecay=0.9, exploration=1.0, explorationdecay=0.9)
+
+train(p1, p2, rounds=100, battles=100)
+
+sys.exit()
+
+
+# Working QPlayer versus RandomPlayer
+p1 = QPlayer(lr=0.9, lrdecay=0.95, exploration=0.1, explorationdecay=0.99)
 p2 = RandomPlayer()
 
-game = TicTacToe(p1, p2)
-count, p1wins, p2wins, draws = game.train(100, 100)
+train(p1, p2, rounds=100, battles=100)
 
-if p1.wins > p2.wins:
-    bestQPlayer = p1
-    print("Player 1 was the best player")
-else:
-    bestQPlayer = p2
-    print("Player 2 was the best player")
-bestQPlayer.savePolicy()
-
-plt.figure()
-plt.ylabel('Game outcomes in %')
-plt.xlabel('Game number')
-
-plt.plot(count, p1wins, 'g-', label='{} wins'.format(p1.name))
-plt.plot(count, p2wins, 'b-', label='{} wins'.format(p2.name))
-plt.plot(count, draws, 'r-', label='Draw')
-
-plt.grid(True)
-plt.legend(loc='best', shadow=True, fancybox=True, framealpha=0.7)
-plt.draw()
+sys.exit()
 
 
-p3 = AlphaBetaPlayer()
+# Working QPlayer versus AlphaBetaPlayer
+p1 = QPlayer(lr=0.7, lrdecay=0.95, exploration=0.3, explorationdecay=0.95)
+p2 = AlphaBetaPlayer()
 
-game = TicTacToe(bestQPlayer, p3)
-count, p1wins, p2wins, draws = game.test(50, 5)
+train(p1, p2, rounds=50, battles=50)
 
-plt.figure()
-plt.ylabel('Game outcomes in %')
-plt.xlabel('Game number')
+p1.savePolicy()
+sys.exit()
 
-plt.plot(count, p1wins, 'g-', label='{} wins'.format(bestQPlayer.name))
-plt.plot(count, p2wins, 'b-', label='{} wins'.format(p3.name))
-plt.plot(count, draws, 'r-', label='Draw')
 
-plt.grid(True)
-plt.legend(loc='best', shadow=True, fancybox=True, framealpha=0.7)
-plt.show()
+# Bayesian optimisation of parameters
+from skopt import gp_minimize
+def objective(args):
+    lr, lrdecay, exploration, explorationdecay = args
+    p1 = QPlayer(lr, lrdecay, exploration, explorationdecay)
+    p2 = AlphaBetaPlayer()
+
+    game = TicTacToe(p1, p2)
+    count, p1wins, p2wins, draws = game.train(50, 5)
+    score = sum(p1wins) + sum(draws) - sum(p2wins)
+    return -score
+
+res = gp_minimize(objective, [(0.0, 1.0), (0.0, 1.0), (0.0, 1.0), (0.0, 1.0)], acq_func="LCB", n_calls=10, n_random_starts=5, noise=0.1**2)
+print("lr={}, decay={}, exploration={}, decay={}, f()={}".format(res.x[0], res.x[1], res.x[2], res.x[3], res.fun))
